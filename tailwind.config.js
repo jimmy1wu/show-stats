c27962f672a30c93055d966c2cb7f5a4d16f96ad
Jimmy Wu <jimmywu0605@gmail.com>
2022-01-10 23:51:32 -0500

    fix tailwind purging dynamic class bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    safelist: ["rotate-6", "-rotate-6"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
