5b82380482443d82f1ff805f95b13f520b4affff
Jimmy Wu <jimmywu0605@gmail.com>
2022-01-18 21:57:21 -0500

    update cra and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  purge: {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-    safelist: ["rotate-6", "-rotate-6"],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  safelist: ["rotate-6", "-rotate-6"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
