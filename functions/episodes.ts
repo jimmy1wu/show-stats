d0f85d0d7baff53f2e75056e08d853ffee2d5a15
Jimmy Wu <jimmywu0605@gmail.com>
2022-01-22 23:58:43 -0500

    use promise all to get ep's for each season

--- functions/episodes.ts
+++ functions/episodes.ts
@@ -6,16 +6,20 @@ import { Episode } from "../src/models";
 export const handler: Handler = async (event, context) => {
   const { imdbID } = event.queryStringParameters;
 
-  const ratings = JSON.parse(await redis.get(imdbID));
-
   const { title, totalSeasons } = await getShowById(imdbID);
 
-  const episodes: Episode[] = [];
+  const promises: Promise<Episode[]>[] = [];
   for (let season = 1; season <= totalSeasons; season++) {
-    const seasonEpisodes = await getSeasonEpisodes(imdbID, season);
-    episodes.push(...seasonEpisodes);
+    promises.push(getSeasonEpisodes(imdbID, season));
   }
+  const data = await Promise.all(promises);
 
+  const episodes: Episode[] = [];
+  data.forEach((seasonEpisodes) => {
+    episodes.push(...seasonEpisodes);
+  });
+
+  const ratings = JSON.parse(await redis.get(imdbID));
   episodes.forEach((episode) => {
     episode.imdbRating = ratings[episode.imdbID];
   });
