22fe1fdfc7b2ae9c24994c0b2eb47be08078ccd6
Jimmy Wu <jimmywu0605@gmail.com>
2022-02-22 21:07:06 -0500

    use react-router-dom link component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 import { Container } from ".";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 
 const Header = () => {
-  const history = useHistory();
-
   return (
     <header className="bg-blue-700 py-2.5">
       <Container>
-        <button
-          className="cursor-pointer text-2xl text-gray-50"
-          onClick={() => history.push("/")}
-        >
+        <Link to="/">
           <Logo />
-        </button>
+        </Link>
       </Container>
     </header>
   );
