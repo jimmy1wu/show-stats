574abd7b6c972e6301afea29e2f68c63c9c4c5ba
Jimmy Wu <jimmywu0605@gmail.com>
2023-01-08 17:55:36 -0500

    use inter font

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Header } from ".";
 
 const Layout = ({ children }: PropsWithChildren<{}>) => {
   return (
-    <div className="flex flex-col min-h-screen bg-blue-50">
+    <div className="flex flex-col min-h-screen bg-blue-50 font-inter">
       <Header />
       <main className="flex-grow">
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   safelist: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
